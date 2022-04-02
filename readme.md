c637330376c273eb04a058f8766dac4b39d296de
Anderson Souza <andersonpds14@gmail.com>
2022-04-01 23:45:47 -0300

    Adiciona descrição do segundo dia do desafio; Implementa parse do Json, buscando resultados através do atributo do JSON;

--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 # 7 Days Of Code - Day one
 
-## Description
+## Description - Day 1
 
 O que você vai precisar fazer:
 - Criar uma conta no IMDB para ter a chave de acesso ao serviço (apiKey), mas cuidado, essa chave não deve ser commitada no Github ou em outro repositório!
 - Criar o código Java que executará uma requisição HTTP do tipo GET. Você pode usar o pacote java.net.http e as classes HttpRequest, HttpClient e HttpResponse, além da classe URI
 - Executar a requisição e pegar a resposta (o JSON)
 - Imprimir o corpo da resposta no console
+
+## Description - Day 2
+A sua tarefa de hoje será parsear a resposta obtida no dia anterior. Em outras palavras, você vai extrair as informações desse JSON.
+Sua tarefa será extrair o título do filme e a URL da imagem a partir da resposta JSON.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -8,42 +7,53 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
 
-        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("application.properties")) {
-
-            Properties properties = new Properties();
-
-            properties.load(input);
-
-            String url = properties.getProperty("IMBD_URL");
-            String key = properties.getProperty("IMBD_KEY");
-
-            System.out.println(url);
-            System.out.println(key);
-
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-
         try {
+
+            final String imbdUrl = PropertyManager.getPropertyValueByKey("IMBD_URL");
+            final String imbdKey = PropertyManager.getPropertyValueByKey("IMBD_KEY");
+
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(""))
-                    .timeout(Duration.of(10, ChronoUnit.SECONDS))
-                    .GET()
-                    .build();
+                .uri(new URI(imbdUrl + imbdKey))
+                .timeout(Duration.of(10, ChronoUnit.SECONDS))
+                .GET()
+                .build();
 
             HttpResponse<String> response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
 
             System.out.println(response.body());
+
+            List<String> title = getJsonContentByAttribute(response.body(), "title");
+            List<String> image = getJsonContentByAttribute(response.body(), "image");
+
+            System.out.println(title);
+            System.out.println(image);
         } catch (URISyntaxException | IOException | InterruptedException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static List<String> getJsonContentByAttribute(String httpResponse, String attribute) {
+        final String regex = "(?<=\"" + attribute + "\":\")(.*?)(?=\")";
+
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(httpResponse);
+
+        List<String> result = new ArrayList<>();
+
+        while (matcher.find()) {
+            result.add(matcher.group(0));
+        }
+
+        return result;
     }
 
 }
